2b1495cf2458e0a52222b61b99586a35b31f01a0
Domenico Leone <domenico.leone@gmail.com>
2019-04-21 11:13:07 +0200

    added version to cookie value

--- assets/js/privacy-cookies.js
+++ assets/js/privacy-cookies.js
@@ -5,6 +5,7 @@ var ck = {
         cname: '_mrw_pck',
         cdomain: document.domain,
         cvalue: 1,
+        cversion: '20190420',
         cpath: '/',
         exdays: 30,
         cookie: ''
@@ -14,21 +15,23 @@ var ck = {
         if((opt.cname in c) == false || (opt.cname in c) == 'undefined')
             opt.target.fadeIn(2000);
     },
-    setCookies: function(cname, cvalue, domain, cpath, exdays) {
+    setCookies: function(cname, cvalue, cversion, domain, cpath, exdays) {
         $('a#accept').bind('click', function(){
-            Cookies.set(cname, {
-                value: cvalue,
-                domain: domain,
-                expires: exdays,
-                path: cpath
-            });
+            var value = btoa({value: cvalue, version: cversion});
+            Cookies.set(cname,
+                value,
+                {
+                    domain: domain,
+                    expires: exdays,
+                    path: cpath
+                });
             opt.target.fadeOut('fast');
         })
     },
     init: function(){
         opt = ck.options;
         ck.checkCookies();
-        ck.setCookies(opt.cname, opt.cvalue, opt.cdomain, opt.cpath, opt.exdays);  
+        ck.setCookies(opt.cname, opt.cvalue, opt.cversion, opt.cdomain, opt.cpath, opt.exdays);  
     }
 }
 $(document).ready(function(){
