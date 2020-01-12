3fe6ad22024cd1d22b1d3c0bc87327a7c534099c
Domenico Leone <domenico.leone@gmail.com>
2020-01-12 16:08:38 +0100

    added notify.js + custom CSS + updated layouts

---
+++ assets/js/notify.js
@@ -0,0 +1,35 @@
+var notify = {
+    options: {
+        target: $('#notify'),
+    },
+    message: function(message, type){
+        if(opt.target.hasClass('alert alert-*'));
+            opt.target.removeClass('alert alert-*');
+
+        switch(type) {
+            case 'info':
+                opt.target.addClass('alert alert-info');
+                break;
+            case 'success':
+                opt.target.addClass('alert alert-success');
+                break;
+            case 'warning':
+                opt.target.addClass('alert alert-warning');
+                break;
+            case 'danger':
+                opt.target.addClass('alert alert-danger');
+                break;
+            default:
+                opt.target.addClass('alert alert-secondary');
+          }
+          var p = opt.target.children();
+          p.text(message);
+          opt.target.fadeIn(2000);
+    },
+    init: function(){
+        opt = notify.options;
+    }
+}
+$(document).ready(function(){
+    notify.init();
+});
